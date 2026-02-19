74b7e164f472d3e76a421bc448fe0e9c26254efa
Dusa Edmond <edmond.dusa@sap.com>
2026-02-19 22:35:04 +0100

    Fixed pahe tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { I18nProvider } from './context/I18nContext';
+import { useEffect } from 'react';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Pricing from './pages/Pricing';
 import Contact from './pages/Contact';
 
+function PlausibleTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Track page view on route change
+    if (window.plausible) {
+      window.plausible('pageview');
+    }
+  }, [location.pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <I18nProvider>
       <BrowserRouter>
+        <PlausibleTracker />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
